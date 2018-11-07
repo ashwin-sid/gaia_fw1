a4255f63105565466c56aa778164c1a2fa846f10
Ashwini Sidhu <30416759+ashwin-sid@users.noreply.github.com>
2018-11-07 13:16:23 +0000

    Update config-clish.cmd

--- Sample-Playbooks/config-clish.cmd
+++ Sample-Playbooks/config-clish.cmd
@@ -1,5 +1,5 @@
-lock database override
-set static-route 1.1.1.1/32 nexthop gateway address 192.168.1.2 on
-set static-route 1.1.1.2/32 nexthop gateway address 192.168.1.2 on
-set static-route 1.1.1.3/32 nexthop gateway address 192.168.1.2 on
-save config
+#lock database override
+#set static-route 1.1.1.1/32 nexthop gateway address 192.168.1.2 on
+#set static-route 1.1.1.2/32 nexthop gateway address 192.168.1.2 on
+#set static-route 1.1.1.3/32 nexthop gateway address 192.168.1.2 on
+#save config
