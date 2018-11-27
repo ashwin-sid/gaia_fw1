94e6a477dd64a30c35f53e31bc6cba5820a437a9
Ashwini Sidhu <asidhu@lon-sec-01.cliffordchance.com>
2018-11-27 14:32:58 +0000

    Playbook names alligned and bash configure commands playbook added

---
+++ Sample-Playbooks/configure-clish.cmd
@@ -0,0 +1,5 @@
+#lock database override
+#set static-route 1.1.1.1/32 nexthop gateway address 192.168.1.2 on
+#set static-route 1.1.1.2/32 nexthop gateway address 192.168.1.2 on
+#set static-route 1.1.1.3/32 nexthop gateway address 192.168.1.2 on
+#save config
